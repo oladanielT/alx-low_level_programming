c9fc5d4bc4e07226c29d9d8d769351135978e7af
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-08 07:01:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is my printf function;
+ * Return: Always return 0;
+ */
+int main(void)
+{
+	char myString[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(myString);
+	return (0);
+}
