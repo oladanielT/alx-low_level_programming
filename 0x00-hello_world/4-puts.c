84b61e81dd1dbd318e6141034fc2ebdc1a371de8
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-08 06:47:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is my first main function
+ * Return: Always 0;
+ */
+int main(void)
+{
+	char myString[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myString);
+	return (0);
+
+}
