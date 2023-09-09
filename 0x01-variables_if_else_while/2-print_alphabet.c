91191e45b8669f58e6a2f14439424cd1a8580a04
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-09 15:46:27 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-    char i;
+	char i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    putchar(i);
-    putchar('\n');
+	for (i = 'a'; a <= 'b'; i++)
+		putchar(i);
+		putchar('\n');
 
-    return (0);
+	return (0);
 }
