80cae68aa2b720e4a92a2100b686851557fdc4f0
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-10 11:10:34 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,17 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 10; j++)
+	for (j = i; j < 10; j++)
 	{
 	if (i != j)
 	{
 	putchar('0' + i);
 	putchar('0' + j);
-	}
-	if (i != 8 && j != 9)
+	if (i != 8 || i != 9)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
