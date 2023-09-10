1d90f52d76f918ffd9297a6770b60f79327daf14
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-10 09:49:30 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,3 +3,23 @@
  * main - main function
  * Return: Always 0 (success)
  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = i; j < 10; j++)
+	{
+	if (i != j)
+	{
+	putchar('0' + i);
+	putchar('0' + j);
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
