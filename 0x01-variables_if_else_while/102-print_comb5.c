2df692903f78c29712a5047b4db1ba2b2b16cc17
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-10 14:36:09 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - combination of two two integers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 98; i++)
+	{
+	for (j = 0; j < 98; j++)
+	{
+	putchar('0' + i);
+	putchar('0' + i);
+	putchar(' ');
+	putchar('0' + j);
+	putchar('0' + j);
+	if (i == 98 && j == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
