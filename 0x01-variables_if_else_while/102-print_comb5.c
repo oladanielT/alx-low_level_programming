468de1ebc49035fc32f6f0b5ef5f356cac1bd5c8
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-10 21:33:07 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i <= 98; i++)
 	{
-	for (j = i + 1; j < 99; j++)
+	for (j = i + 1; j <= 99; j++)
 	{
-	putchar('0' + i);
-	putchar('0' + i);
+	putchar('0' + (i / 10));
+	putchar('0' + (i % 10));
 	putchar(' ');
-	putchar('0' + j);
-	putchar('0' + j);
+	putchar('0' + (j / 10));
+	putchar('0' + (j % 10));
 	if (i == 98 && j == 99)
 	continue;
 	putchar(',');
