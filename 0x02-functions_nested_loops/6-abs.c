1f9e03b2df08be0d7cde3a0063d5ef8a395dc5cd
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-13 16:43:00 -0700

    6-abs.c, checking the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Getting the absolute value of a number
+ * Return: Always (0)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else if (n > 0)
+		return (n);
+	return (0);
+}
