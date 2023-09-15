1c854cb74059a170dbd89cbbb22583e220c1cfde
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-15 05:12:30 -0700

    11-print_to_98.c:

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function to print numbers
+ * @n: integer to check
+ * Return: Always (0);
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+		printf("%d, ", n);
+	}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else
+	{
+	for (; n <= 98; n--)
+	{
+		printf("%d, ", n);
+	}
+	}
+}
