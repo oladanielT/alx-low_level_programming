05849f47bd49fd52e82eee64de64e0285e99e6c9
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-15 08:55:50 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	int sum = 1;
+	int sum = 0;
 	int i;
 
 	for (i = 0; i <= 1024; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		sum *= i;
+		if (i % 3 == 0 || i % 5 == 0)
+		sum += i;
 	}
 	printf("%d \n", sum);
 	return (0);
