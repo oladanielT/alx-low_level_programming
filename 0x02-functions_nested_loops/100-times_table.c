687924b1c93e6e81aafd418922ef267293533d61
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-15 06:22:23 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - function to print times table
+ * @n: Integer to print
+ * Return: Always (0)
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+	for (a = 0; a <= n; a++)
+	{
+	for (b = 0; b <= n; b++)
+	{
+	c = a * b;
+	if (b == n)
+	{
+		printf("%d", c);
+	}
+	else
+	{
+	printf("%d, ", c);
+	}
+	}
+	printf("\n");
+	}
+	}
+}
+
