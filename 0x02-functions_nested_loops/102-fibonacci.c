c097ecedf8a615454546dd67c0345645c73e9410
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-15 14:23:44 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - fibonacci function
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int i = 0;
+	int next;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%d", a);
+		}
+		else if (i == 1)
+		{
+			printf(", %d", b);
+		}
+		else
+		{
+			b += a;
+			a = b - a;
+			printf(", %d", b);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
