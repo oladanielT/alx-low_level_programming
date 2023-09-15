43635585afcdc26ea2d418899c3423ce6cde1d6e
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-15 08:49:09 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function to add numbers
+ * Return: Always (0);
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		sum += i;
+	}
+	printf("%d \n", sum);
+	return (0);
+}
