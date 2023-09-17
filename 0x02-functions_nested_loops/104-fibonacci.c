7229b412b96f1a8e91cc24d63ea840ffe7bab515
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-17 07:51:33 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 	int count;
 	unsigned long a = 0, b = 1, sum;
 	unsigned long h_1, h_2;
-	unsigned long a_h_1, a_h_2, b_h_1, b_h_2;
+	unsigned long a_h_1,a_h_2, b_h_1, b_h_2;
 
 	for (count = 0; count < 92; count++)
 	{
@@ -18,8 +18,8 @@ int main(void)
 		b = sum;
 	}
 	a_h_1 = a / 10000000000;
-	a_h_2 = a % 10000000000;
 	b_h_1 = b / 10000000000;
+	a_h_2 = a % 10000000000;
 	b_h_2 = b % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
