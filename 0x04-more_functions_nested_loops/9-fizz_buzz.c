83d9808a19c206a69cf9d3f08c233fcc5dec3561
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-19 18:17:37 -0700

    function that prints fuzz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 99; i++)
+	for (i = 1; i <= 99; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
