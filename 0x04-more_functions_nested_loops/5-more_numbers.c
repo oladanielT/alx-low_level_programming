93d63779f74574c91988f5f21813c4a732c13785
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-19 17:30:44 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar('0' + (j / 10));
-			if (j > 9)
+			if (j >= 10)
+			{
+				_putchar('0' + (j / 10));
+			}
 			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
-	}}
+	}
+}
