71a6ac1a0730d79a92cd5400889bb98c42a48965
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-21 01:37:14 -0700

    function that prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line.
+ * print_rev - function that prints a string
+ * in reverse, followed by a new line.
  * @s: string to print
  * Return: Always 0
  */
