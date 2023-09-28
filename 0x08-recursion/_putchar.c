e25c4d5c4e5bda26ebd0011388e5f02b0fa704e5
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-28 15:15:03 -0700

    function that prints a string, followed by a new line.

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - function to print one character
+ * @c: character to print
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
