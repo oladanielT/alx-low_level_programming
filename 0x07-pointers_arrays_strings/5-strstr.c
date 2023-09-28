7749d5228b40580754528c0be1ff127df417e1d7
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-09-28 12:23:19 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,21 +7,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
+	if (needle == '\0')
 	{
 		return (haystack);
 	}
-	while (*haystack != '\0')
+	while (haystack != '\0')
 	{
-		char *h = haystack;
-		char *n = needle;
+		char h = haystack;
+		char n = needle;
 
-		while (*n != '\0' && *h == *n)
+		while (n != '\0' && h == n)
 		{
 			h++;
 			n++;
 		}
-		if (*n == '\0')
+		if (n == '\0')
 		{
 			return (haystack);
 		}
