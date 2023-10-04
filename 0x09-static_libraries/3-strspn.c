862d8a24592afef497a73c2c5c09c0d77a2ed95b
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-10-04 01:27:26 -0700

    Updated

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -17,12 +17,12 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			if (*s == accept[i])
 			{
-				n++;
+				j++;
 				break;
 			}
 			else if (accept[i + 1] == '\0')
 			{
-				return (n);
+				return (j);
 			}
 		}
 		s++;
