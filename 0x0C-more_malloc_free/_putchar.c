a7c808fa406586db98e7865ebdf3c0e8ad8e04e8
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-10-07 06:01:01 -0700

    function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - function to print a character
+ * @c: character to print
+ * Return: Always 0
+ */
+
+int _putchar(char *c)
+{
+	return (write(1, &c, 1));
+}
