306ae57d5fcbe591ef1c02284922e20cf9409904
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-06 12:33:44 -0800

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,4 +3,34 @@
 #include "lists.h"
 
 /**
- *
+ * add_node - function to add a new element to a linked list
+ * @head: first element of the linked list
+ * @str: the linked list
+ * Return: the address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+	{
+		len++;
+
+		new = malloc(sizeof(list_t));
+
+		if (!new)
+		{
+			return (NULL);
+		}
+		else
+		{
+			new->str = strdup(str);
+			new->len = len;
+			new->next = (*head);
+			(*head) = new;
+		}
+	}
+	return (*head);
+}
