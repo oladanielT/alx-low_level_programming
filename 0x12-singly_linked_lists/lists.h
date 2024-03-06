56459e19ae1ad4182a9c68c1e9ab54bb55629d8b
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-06 08:25:00 -0800

    Updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct list_s - function to define our struct
+ * @str: string
+ * @len: length
+ * @next: next node
+ * Description: singly linked list structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
