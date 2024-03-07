4e2f90f1abf4ac39b077f03ea16ed9e2eaa5fe8a
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-07 07:57:45 -0800

    a function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
-
 
 /**
  * free_list - function to free a list
  * @head: the pointer to the first element of the list
  * Return: Always 0
  */
-
 
 void free_list(list_t *head)
 {
