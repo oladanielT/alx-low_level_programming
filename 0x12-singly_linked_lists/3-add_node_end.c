95614f7510c402ae536d25bff1cd8af2f6149020
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-07 05:48:13 -0800

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * add_node_end - function to add elemnt to the end of a node
+ * @head: first element in a node
+ * @str: the element in a linked list
+ * Return: the address to the end element in a node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t len = 0;
+	list_t *new;
+	list_t promp = *head;
+
+	while (str[len])
+	{
+		len++;
+	}
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (promp->next)
+	{
+		promp = promp->next;
+		promp->next = new;
+
+		return (new);
+	}
+}
+
