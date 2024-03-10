33a7cab871a3741f577d978cd8c6951968c294ce
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-10 13:27:59 -0700

    Updated

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,13 +11,16 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (1);
-	}
-	else if (y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-	return (x * power(y - 1));
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
