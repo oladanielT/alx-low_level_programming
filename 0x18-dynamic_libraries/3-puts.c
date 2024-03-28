ee4b60798f9749b38cc57504b5eb19c9e3de89c0
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-28 10:20:47 -0700

    Script to create a dynamic library

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-void _puts(char *str);
-#include <stdio.h>
 
 /**
- * main - funtion to print using puts
+ * _puts - function that prints a string
+ * @str: string to print
  * Return: Always 0
  */
 
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        int i;
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                _putchar(str[i]);
+        }
+        _putchar('\n');
 }
