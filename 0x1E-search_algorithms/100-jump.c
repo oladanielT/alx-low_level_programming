a10f96da0f88a40c535d492643f922067ac829b2
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-17 10:12:42 -0700

    the time complexity (average case) of a jump search in a skip list of size n, with an express lane using step = sqrt(n)?

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
 #include <math.h>
 #include "search_algos.h"
+
 /**
- * jump_search - function that searches for a value in a sorted array
- * @array: array of elements to search
- * @size: numbers of ment in array
- * @value: value to search
- * Return: value if found or -1 if not found
+ * jump_search - function to search for the index ofan element.
+ * @array: list of elements
+ * @size: number of elements in the array
+ * @value: value to search.
+ * Return: -1 if not found and index of the value if found.
  */
+
 
 int jump_search(int *array, size_t size, int value)
 {
-	int i, j;
-	int l = 0;
-	int step = sqrt((int)(size));
-	int h = step - 1;
-
-	if (!array || size == 0)
-		return (-1);
-	while (j < (int)(size))
+	int step = (int) sqrt(size);
+	int left = 0;
+	int i;
+	int right = step - 1;
+	
+	while (right < (int)size)
 	{
-		printf("Value checked array[%d} = [%d]\n", l, array[l]);
-		for (i = l; i <= h; i++)
+		printf("Value checked array[%d] = [%d]\n", right, array[right]);
+		if (array[right] >= value)
 		{
-			if (array[i] == value)
-				return (i);
+			for (i = left; i <= right; ++i)
+			{
+				printf("Value checked array[%d] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+					return i;
+			}
+			return -1;
 		}
-		l = h + 1;
-		h = h + step;
-
-		if (h >= (int)(size))
-			return (-1);
-		j++;
+		
+		left = right + 1;
+		right = right + step;
+		
+		if (right >= (int)size)
+			right = size - 1;
 	}
-
-	return (-1);
+	for (i = left; i < (int)size; ++i)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return i;
+	}
+	
+	return -1;
 }
